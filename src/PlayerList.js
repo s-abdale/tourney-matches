ee774f9f3da1d928ab7f29c3908e0dbf84bdc31c
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 23:10:17 -0500

    Added starter code for components

---
+++ src/PlayerList.js
@@ -0,0 +1,12 @@
+import React from 'react'; // optional
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {/* Players will be shown here */}
+    </section>
+  )
+}
+
+export default PlayerList;
