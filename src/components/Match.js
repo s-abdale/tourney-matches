51c1d4fa152a8184dcb381182bf7089904647815
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 00:14:35 -0500

    Refactored Match function using short circuit evaluation

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,27 +1,15 @@
 import React from 'react'; // optional
 
 function Match(props) {
-  if(props.winner) {
-    {/* To be shown when there is a winner */}
-    return (
-      <article className="Match">
-        <h1>
-          {props.players[0]} <span>vs</span> {props.players[1]}
-        </h1>
-        <h2>{props.winner} is the winner by {props.scoreDifference}!</h2>
-      </article>
-    )
-  } else {
-    {/* To be shown when there is no winner */}
-    return (
-      <article className="Match">
-        <h1>
-          {props.players[0]} <span>vs</span> {props.players[1]}
-        </h1>
-        <h2>No winners yet!</h2>
-      </article>
-    )
-  };
+  const { players, winner, scoreDifference } = props;
+
+  return (
+    <article className="Match">
+      <h1>{players[0]} <span>vs</span> {players[1]}</h1>
+      {winner && <h2>{winner} is the winner by {scoreDifference}!</h2>}
+      {!winner && <h2>No winners yet!</h2>}
+    </article>
+  );
 }
 
 export default Match;
