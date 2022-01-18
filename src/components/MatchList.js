94e1c226e3f8e25b3aca71021c6241244d9956e9
Safia Abdale <safia.abdale@gmail.com>
2022-01-17 23:35:17 -0500

    Created mock data, imported to the MatchList and PlayerList components

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,5 +1,6 @@
 import React from 'react'; // optional
 import Match from "./Match"; // plugging this in to PlayerList component
+import matchData from "../data/matchData"; // match data
 
 
 function MatchList() {
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,5 +1,8 @@
 import React from 'react'; // optional
 import Player from "./Player"; // plugging this in to PlayerList component
+import playerData from "../data/playerData"; // player data
+import matchData from "../data/matchData"; // match data
+
 
 
 function PlayerList() {
