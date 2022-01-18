6c351ffe5527556cfa01f89a2e3356a8f44f9fe1
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 11:17:36 -0500

    Refactored playerData and matchData usage

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,14 +1,16 @@
 import React from 'react'; // optional
 import Match from "./Match"; // plugging this in to PlayerList component
-import matchData from "../data/matchData"; // match data
+// import matchData from "../data/matchData"; // match data
 
 
 function MatchList(props) {
   const oneMatch = matchData[0];
+  const {matchData} = props;
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
       <Match {...oneMatch}/>
+      {/* {matchData} */}
     </section>
   )
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,14 +1,15 @@
 import React from 'react'; // optional
 import Player from "./Player"; // plugging this in to PlayerList component
 import playerData from "../data/playerData"; // player data
-import matchData from "../data/matchData"; // match data
+// import matchData from "../data/matchData"; // match data
 import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers'; // helper functions
 
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const preparedPlayerData = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(preparedPlayerData, matchData);
   const onePlayer = parsedPlayerData[0];
+  const {matchData, playerData} = props;
 
 
   return (
